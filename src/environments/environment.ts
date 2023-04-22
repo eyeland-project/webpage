fab20f1ad29fb99b5751714f05136843065b817c
CamiloSinningUN <camilosinning.cs@gmail.com>
2023-04-21 20:01:27 -0500

    new api domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	apiUrl: 'https://eyeland-api-production.up.railway.app/api/teachers',
+	apiUrl: 'https://salamanca-island-mobile-api-production.up.railway.app/api/teachers',
 	// apiUrl: 'http://localhost:3000/api/teachers',
-	socketUrl: 'https://eyeland-api-production.up.railway.app/teachers'
+	socketUrl: 'https://salamanca-island-mobile-api-production.up.railway.app/teachers'
 };
