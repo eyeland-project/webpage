cd8b9065160bb0cb70fb029539008d07002985f7
CamiloSinningUN <camilosinning.cs@gmail.com>
2023-06-09 11:07:00 -0500

    changing to production variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
 	// PRODUCTION
-	// apiTeacherUrl:
-	// 	'https://salamanca-island-mobile-api-production.up.railway.app/api/teachers',
-	// apiAdminUrl:
-	// 	'https://salamanca-island-mobile-api-production.up.railway.app/api/admin',
-	// socketUrl:
-	// 	'https://salamanca-island-mobile-api-production.up.railway.app/teachers'
+	apiTeacherUrl:
+		'https://salamanca-island-mobile-api-production.up.railway.app/api/teachers',
+	apiAdminUrl:
+		'https://salamanca-island-mobile-api-production.up.railway.app/api/admin',
+	socketUrl:
+		'https://salamanca-island-mobile-api-production.up.railway.app/teachers'
 
 	// DEVELOPMENT
-	apiTeacherUrl: 'http://localhost:3000/api/teachers',
-	apiAdminUrl: 'http://localhost:3000/api/admin',
-	socketUrl: 'http://localhost:3000/teachers'
+	// 	apiTeacherUrl: 'http://localhost:3000/api/teachers',
+	// 	apiAdminUrl: 'http://localhost:3000/api/admin',
+	// 	socketUrl: 'http://localhost:3000/teachers'
 };
