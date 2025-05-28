72746fbcb52327377f551b8b9776c838d8e82459
Jean Pierre <jalvarezidep@gmail.com>
2025-05-28 12:20:11 -0500

    Local variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
 	// PRODUCTION
-	apiTeacherUrl: 'https://eyeland-server-afgw.onrender.com/api/teachers',
-	apiAdminUrl: 'https://eyeland-server-afgw.onrender.com/api/admin',
-	socketUrl: 'https://eyeland-server-afgw.onrender.com/teachers'
+	// apiTeacherUrl: 'https://eyeland-server-afgw.onrender.com/api/teachers',
+	// apiAdminUrl: 'https://eyeland-server-afgw.onrender.com/api/admin',
+	// socketUrl: 'https://eyeland-server-afgw.onrender.com/teachers'
 
 	// DEVELOPMENT
-	// 	apiTeacherUrl: 'http://localhost:3000/api/teachers',
-	// 	apiAdminUrl: 'http://localhost:3000/api/admin',
-	// 	socketUrl: 'http://localhost:3000/teachers'
+		apiTeacherUrl: 'http://localhost:3000/api/teachers',
+		apiAdminUrl: 'http://localhost:3000/api/admin',
+		socketUrl: 'http://localhost:3000/teachers'
 };
