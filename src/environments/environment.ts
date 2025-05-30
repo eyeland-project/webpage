da7464e90f456753ce656857f1e4ae316c0aa6d6
Jean Pierre <jalvarezidep@gmail.com>
2025-05-30 00:56:24 -0500

    Dist removed from gitignore for deployment and enviroments.ts updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,13 @@ export const environment = {
 	// apiAdminUrl: 'https://eyeland-server-afgw.onrender.com/api/admin',
 	// socketUrl: 'https://eyeland-server-afgw.onrender.com/teachers'
 
+	//SERVIDOR EN AZURE
+	apiTeacherUrl: 'https://eyeland-backend-cjedd2hzfeccdhb8.canadacentral-01.azurewebsites.net/api/teachers',
+	apiAdminUrl: 'https://eyeland-backend-cjedd2hzfeccdhb8.canadacentral-01.azurewebsites.net/api/admin',
+	socketUrl: 'https://eyeland-backend-cjedd2hzfeccdhb8.canadacentral-01.azurewebsites.net/teachers'
+
 	// DEVELOPMENT
-		apiTeacherUrl: 'http://localhost:3000/api/teachers',
-		apiAdminUrl: 'http://localhost:3000/api/admin',
-		socketUrl: 'http://localhost:3000/teachers'
+	//	apiTeacherUrl: 'http://localhost:3000/api/teachers',
+	//	apiAdminUrl: 'http://localhost:3000/api/admin',
+	//	socketUrl: 'http://localhost:3000/teachers'
 };
